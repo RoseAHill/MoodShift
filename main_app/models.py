515552421feada03455825bd266cf5946deb5fa1
Rose Aranaga-Hill <rosearanagahill@gmail.com>
2021-08-28 18:03:14 -0400

    make mood and finish entry models

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,15 @@
 from django.db import models
-
+from datetime import date
+from django.contrib.auth.models import User
 # Create your models here.
 
+class Mood(models.Model):
+  alias = models.CharField
+  reaction = models.CharField
+  faicon = models.CharField
+
 class Entry(models.Model):
-  date = models.DateField
+  entryDate = models.DateField
   description = models.TextField(max_length=1000)
+  mood = models.ForeignKey(Mood, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
